a8e7fa0777ffc90a91ba942ecdee5f1a3382585d
jwathaah <ah@jwatha.com>
2025-12-03 11:17:51 +0300

    Update id_generator.py

--- utils/id_generator.py
+++ utils/id_generator.py
@@ -1,1 +1,9 @@
-# id_generator.py created based on project structure
+import uuid
+
+def generate_uuid():
+    """
+    إنشاء معرف فريد عشوائي (UUID Version 4).
+    يستخدم هذا المعرف كـ Primary Key لجميع الجداول (Users, Sections, Content...)
+    لضمان عدم تكرار الأرقام حتى لو قام شخصان بالإضافة في نفس الوقت.
+    """
+    return str(uuid.uuid4())
