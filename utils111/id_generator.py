912bcc5de4689c85ee44598be4e84d5bd25aa2ab
jwathaah <ah@jwatha.com>
2025-12-07 10:45:36 +0300

    Delete utils111 directory

--- utils111/id_generator.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-def generate_uuid():
-    """
-    إنشاء معرف فريد عشوائي (UUID Version 4).
-    يستخدم هذا المعرف كـ Primary Key لجميع الجداول (Users, Sections, Content...)
-    لضمان عدم تكرار الأرقام حتى لو قام شخصان بالإضافة في نفس الوقت.
-    """
-    return str(uuid.uuid4())
