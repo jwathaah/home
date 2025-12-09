e391a8b9b7bdf4d7d19938801cd4f236d44e645a
jwathaah <ah@jwatha.com>
2025-12-09 14:10:09 +0300

    Update goo.py

--- pages/goo.py
+++ pages/goo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 # الرابط المراد التحويل إليه
-url = "https://script.google.com/a/macros/jwatha.com/s/AKfycbz-th2hB4BH92aOgMyFBuJ1_TJshq73UPjLwjWbwymRziqJJx7Mg4bAmGwSXEvvJT8y/exec"
+url = "https://script.google.com/macros/s/AKfycbzW09dRNF5SJR1RjdLRC1V3uQoY2lA7gjSvtNDiDA2TtVugRyXWPJSGhbXuLmvTOJ-9/exec"
 
 st.write("سيتم تحويلك بعد ثانيتين...")
 
