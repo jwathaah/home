5b456d6e5208542d1771cee0919203e28bf3fcc7
jwathaah <ah@jwatha.com>
2025-12-09 14:13:42 +0300

    Update goo.py

--- pages/goo.py
+++ pages/goo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 # الرابط المراد التحويل إليه
-url = "https://script.google.com/macros/s/AKfycbzW09dRNF5SJR1RjdLRC1V3uQoY2lA7gjSvtNDiDA2TtVugRyXWPJSGhbXuLmvTOJ-9/exec"
+url = "https://script.google.com/macros/s/AKfycbyrb7KQmd-nZRykSdFCQgaKCKziHGaadDWhCJy0zVPtnVoiXwV-5w-48vzhg827lB87/exec"
 
 st.write("سيتم تحويلك بعد ثانيتين...")
 
